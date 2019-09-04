a213441c60a71db31d9a10ae536ee5de99328349
Ma7eer <maher.alkendi2@gmail.com>
2019-09-04 10:56:26 -0700

    changed config ddata for seo

--- data/config.js
+++ data/config.js
@@ -1,18 +1,18 @@
 module.exports = {
-	title: 'gatsby-starter-i18n',
-	logo: 'https://i18n.smakosh.com/favicon/logo-48.png',
-	url: 'https://i18n.smakosh.com',
-	favicon: 'https://i18n.smakosh.com/favicon/logo-48.png',
+	title: 'Onsor Oman',
+	logo: '../src/images/Onser.jpg',
+	url: 'https://onsor.om/',
+	favicon: '../static/favicon/logo-48.png',
 	cover: 'https://i18n.smakosh.com/cover.jpg',
-	legalName: 'Ismail Ghallou',
-	description: 'Gatsby starter with i18n and lot of other cool features',
+	legalName: 'Onsor Oman',
+	description: 'Onsor Oman | coming soon',
 	socialLinks: {
 		twitter: 'https://twitter.com/smakosh',
 		github: 'https://github.com/smakosh',
 		linkedin: 'https://www.linkedin.com/in/ismail-ghallou-630149122/',
 		instagram: 'https://www.instagram.com/smakosh19/',
 		youtube: 'https://www.youtube.com/user/smakoshthegamer',
-		google: 'https://plus.google.com/u/0/+IsmailSmakoshGhallou',
+		google: 'https://plus.google.com/u/0/+IsmailSmakoshGhallou'
 	},
 	googleAnalyticsID: 'UA-88875900-1',
 	themeColor: '#000696',
@@ -22,15 +22,15 @@ module.exports = {
 		twitter: '@smakosh'
 	},
 	address: {
-		city: 'Errachidia',
-		region: 'Drâa-Tafilalet',
-		country: 'Morocco',
-		zipCode: '52000'
+		city: 'Muscat',
+		region: 'Muscat',
+		country: 'Oman',
+		zipCode: '121'
 	},
 	contact: {
-		email: 'ismai23l@hotmail.com',
-		phone: '+212-663-53-27-61'
+		email: 'admin@onsor.om',
+		phone: 'NA'
 	},
-	foundingDate: '2018',
+	foundingDate: '2019',
 	googleVerification: 'xxx'
-}
+};
